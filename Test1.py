909a508ef05f6b2b9f7ab253eee06733073d0953
martijnarentsen <arentsen@hotitem.nl>
2025-04-08 17:18:04 +0000

    bla

--- Test1.py
+++ Test1.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+# Titel van je app
+st.title("Test1.py")
+
+# Laad het databestand
+df = pd.read_csv("exclusieve_schoenen_verkoop_met_locatie.csv")
+
+# Laat de data zien in Streamlit
+st.write("Hier is je verkoopdata:")
+st.dataframe(df)
 
