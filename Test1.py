0b62e60c9b49acb97aef6c8e7b0aaacb1d2d3289
martijnarentsen <arentsen@hotitem.nl>
2025-04-08 19:27:27 +0200

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -1,20 +1,19 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-'''
+
+# Titel van de pagina
+st.title("Dashboard")
+
 # Laad de data
 df = pd.read_csv("exclusieve_schoenen_verkoop_met_locatie.csv")
 
-# Groepeer per locatie
+# Toon de data in de app
+st.write("Overzicht van de data:")
+st.dataframe(df)
+
+# Groepeer de data per locatie
 verkopen_per_locatie = df.groupby('Locatie')['Verkoop'].sum()
 
-# Plot
-fig, ax = plt.subplots()
-verkopen_per_locatie.plot(kind='bar', ax=ax)
-ax.set_title('Verkoop per Locatie')
-ax.set_xlabel('Locatie')
-ax.set_ylabel('Aantal verkopen')
-
-st.pyplot(fig)
-
-'''
+# Maak een eenvoudige staafgrafiek
+st.write("Verkoop per locatie:")
+st.bar_chart(verkopen_per_locatie)
